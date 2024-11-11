cdf835070fd14fd82594da15f777382911c5690a
elvisfinol <e.finol7@gmail.com>
2024-11-11 14:08:37 +0100

    second commit

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,45 @@
+import ArrowIcon from '@/assets/arrow-right.svg';
+import cogImage from '@/assets/cog.png';
+import cylinderImage from '@/assets/cylinder.png'
+import noddleImage from '@/assets/noodle.png'
+
+import Image from 'next/image';
+
+
 export const Hero = () => {
-  return null;
+  return (
+  <section className='pt-8 mb-20 md:mt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip'>
+    <div className="container">
+      <div className='md:flex items-center'>
+      <div className="md:w-[478px]">
+        <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking">
+        Version 2.0 is hero
+        </div>
+        <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
+          Pathway to productivity
+         </h1>
+        <p className="text-xl text-[#010D3E] tracking-tight mt-6">Celebrate the joy of accomplishment with an app designed to track your progress, motivate your efforts, and recognize your successes.
+        </p>
+        <div className="flex gap-1 items-center mt-[30px]">
+          <button className="btn btn-primary">Get for free</button>
+          <button className="btn gap-1">
+            <span>Learn more</span>
+            <ArrowIcon className="h-5 w-5" />
+            </button>
+        </div>
+      </div>
+
+      <div className='mt-20 md:mt-0 md:h-[648px] md:flex-1 relative'>
+        <Image src={cogImage} alt="Cog image" className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0"
+        />
+
+        <Image src={cylinderImage} alt="Cylinder" width={220} height={220} className='hidden md:block -top-8 -left-32 md:absolute'></Image>
+
+        <Image src={noddleImage} alt="Noodle Image" width={220} height={220} className='hidden lg:block absolute top-[524px] left-[448px] rotate-[30deg]'></Image>
+      </div>
+
+    </div>
+    </div>
+  </section>
+  )
 };
